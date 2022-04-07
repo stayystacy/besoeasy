0ccee40c357b80497e7fcdb3623dbede267a586f
besoeasy <besoeasy@users.noreply.github.com>
2022-04-07 21:29:54 +0530

    -------

--- src/components/social.vue
+++ src/components/social.vue
@@ -1,34 +1,32 @@
 <template>
-	<div class="container max-w-4xl mx-auto px-10">
-		<div class="py-20">
+	<div class="py-10 max-w-xl mx-auto">
 			<div class="grid grid-cols-2 md:grid-cols-5">
 				<div class="p-5 flex items-center justify-center col-span-1 lg:col-span-1 transform hover:scale-150 transition duration-1000">
 					<a href="https://github.com/besoeasy">
-						<img src="img/github.png" class="h-32 w-32" />
+						<img src="img/github.png" class="h-16 w-16" />
 					</a>
 				</div>
 				<div class="p-5 flex items-center justify-center col-span-1 lg:col-span-1 transform hover:scale-150 transition duration-1000">
 					<a href="https://www.linkedin.com/in/besoeasy">
-						<img src="img/linkedin.png" class="h-32 w-32" />
+						<img src="img/linkedin.png" class="h-16 w-16" />
 					</a>
 				</div>
 				<div class="p-5 flex items-center justify-center col-span-1 lg:col-span-1 transform hover:scale-150 transition duration-1000">
 					<a href="https://instagram.com/besoeasy">
-						<img src="img/instagram.png" class="h-32 w-32" />
+						<img src="img/instagram.png" class="h-16 w-16" />
 					</a>
 				</div>
 				<div class="p-5 flex items-center justify-center col-span-1 lg:col-span-1 transform hover:scale-150 transition duration-1000">
 					<a href="https://t.me/besoeasy">
-						<img src="img/telegram.png" class="h-32 w-32" />
+						<img src="img/telegram.png" class="h-16 w-16" />
 					</a>
 				</div>
 
 				<div class="p-5 flex items-center justify-center col-span-1 lg:col-span-1 transform hover:scale-150 transition duration-1000">
 					<a href="https://steamcommunity.com/id/besoeasy">
-						<img src="img/steam.png" class="h-32 w-32" />
+						<img src="img/steam.png" class="h-16 w-16" />
 					</a>
 				</div>
 			</div>
 		</div>
-	</div>
 </template>
